45f1d453a386ead98df7fa1197d70f80f3458085
Girish Mahale <girishmahale786@gmail.com>
2024-03-20 00:19:07 +0530

    init

---
+++ src/main/java/com/easyml/EasyMlApplication.java
@@ -0,0 +1,13 @@
+package com.easyml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyMlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easyml/EasyMlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
