0b6ea7b709b0c8f1da0c40bf007e5eaafbcae104
sahilgoyal7214 <114812731+sahilgoyal7214@users.noreply.github.com>
2024-05-03 00:37:31 +0530

    added project update,delete,reset

--- src/main/java/com/easyml/service/ProjectService.java
+++ src/main/java/com/easyml/service/ProjectService.java
@@ -16,5 +16,15 @@ public class ProjectService {
     public Project getProject(Long projectId) {
         return projectRepository.findById(projectId).orElse(null);
     }
+    public void updateProject(Project project) {
+        projectRepository.updateProjectById(project.getId(), project);
+    }
+
+    public void deleteProject(Project project) {
+        projectRepository.delete(project);
+    }
+    public void resetProject(Project project) {
+        projectRepository.reset(project);
+    }
 
 }
